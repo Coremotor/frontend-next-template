c4c3d7765fcd7bbf2126ddb4132102242f6b9388
Andrey Demakov <coremotor@outlook.com>
2021-12-14 17:18:03 +0300

    ssr styled-components fix

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,7 +1,7 @@
 import { GetServerSideProps, InferGetServerSidePropsType, NextPage } from 'next'
 import Head from 'next/head'
 import { Layout } from 'app/components/layout'
-import { requestOnServer } from 'api'
+import { serverRequest } from 'api'
 import { I_User } from 'interfaices'
 import { User } from 'app/_pages/user'
 
@@ -24,7 +24,7 @@ export default UserPage
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { userId } = context.query
-  const response = await requestOnServer.get(`/users/${userId}`)
+  const response = await serverRequest.get(`/users/${userId}`)
   const user: I_User = response.data
 
   if (!user) {
